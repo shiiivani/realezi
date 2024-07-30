802069f269e6b787c51cff3b9078cc7418d9e68a
Your Name <you@example.com>
2024-07-30 13:52:39 +0530

    Built mobile size view navbar

--- navbar.js
+++ navbar.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerIcon = document.querySelector(".hamburger-menu");
+  const closeIcon = document.querySelector(".close-icon");
+  const mobileNav = document.querySelector(".mobile-nav");
+
+  hamburgerIcon.addEventListener("click", function () {
+    mobileNav.classList.add("active");
+  });
+
+  closeIcon.addEventListener("click", function () {
+    mobileNav.classList.remove("active");
+  });
+});
+
 const listItems = document.querySelectorAll(".upper-nav li");
 
 listItems.forEach((item) => {
