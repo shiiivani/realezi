be98fcb39f62a80b0767b0155d881bf2ff5b1dd0
Your Name <you@example.com>
2024-08-10 20:15:01 +0530

    Fixed upper navbar, fixed budget range

--- navbar.js
+++ navbar.js
@@ -21,3 +21,22 @@ listItems.forEach((item) => {
     this.classList.add("active");
   });
 });
+
+// Upper Nav
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector(".upper-nav-menu-icon");
+  const closeBtn = document.querySelector(".upper-nav-close-icon");
+  const dropdown = document.querySelector(".mobile-view-upper-nav");
+
+  menuBtn.addEventListener("click", function () {
+    menuBtn.classList.add("hidden");
+    closeBtn.classList.remove("hidden");
+    dropdown.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", function () {
+    menuBtn.classList.remove("hidden");
+    closeBtn.classList.add("hidden");
+    dropdown.classList.remove("active");
+  });
+});
